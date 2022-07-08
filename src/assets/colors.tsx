ce0752bb98d7bdf1fd64ea4be8b5171a67a1a1fd
Yunus Gunes <yunusgunes3556@gmail.com>
2022-07-09 01:18:23 +0300

    add shadow

--- src/assets/colors.tsx
+++ src/assets/colors.tsx
@@ -3,5 +3,6 @@ const colors = {
   white: '#FFF',
   grey: '#85929E',
   greySoft: '#AEB6BF',
+  greySoft_2: '#D5D8DC',
 }
 export default colors
--- src/pages/login/styles.tsx
+++ src/pages/login/styles.tsx
@@ -4,7 +4,7 @@ import {colors, numbers} from '../../assets'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.greySoft,
+    backgroundColor: colors.greySoft_2,
     padding: numbers.padding.xl,
     paddingTop: 0,
     alignItems: 'center',
@@ -18,6 +18,16 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     borderBottomLeftRadius: 18,
     borderBottomRightRadius: 18,
+
+    shadowColor: colors.grey,
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+
+    elevation: 24,
   },
   bottomView: {
     flex: 3,
